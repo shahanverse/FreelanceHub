d1bcf0c3ed07a11d2530aa3d54cfbafbf076daad
shahan <shahanverse@gmail.com>
2026-04-25 23:41:57 +0530

    seedAdmin complete

--- server/seedAdmin.js
+++ server/seedAdmin.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
 import dotenv from "dotenv"
-import User from "./model/User";
+import User from "./model/User.js";
 
 dotenv.config()
 //load .mv file
@@ -9,4 +9,40 @@ dotenv.config()
 mongoose.connect(process.env.MONGO_URI)
 //connect to mongodb
 
-const createAdmin
+const createAdmin = async () => {
+      try {
+            const existing = await User.findOne({ role: "admin" })
+            if(existing) {
+                  console.log('Admin already exists ✅');
+                  return process.exit()
+                  
+            }
+
+            //hash admin password 
+            const hashedPassword = await bcrypt.hash("admin123", 10)
+
+            //create admin user 
+
+            await User.create({
+                  name: "Admin", 
+                  email: "admin@freelancehub.com",
+                  password: hashedPassword,
+                  role: "admin",
+                  isVerified: true
+                  // admin is already verified, no OTP needed
+            })
+            console.log("Admin created successfully ✅");
+            console.log("Email: admin@freelancehub.com");
+            console.log("Password: admin123");
+            process.exit()
+            
+            
+
+      } catch (error) {
+            console.log("Error ❌", error.message);
+            process.exit(1)
+            
+      }
+}
+
+createAdmin()
