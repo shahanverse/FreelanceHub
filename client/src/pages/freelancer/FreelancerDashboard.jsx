e55dd2a5549042dfc0e8aee44b38fe5ef3047dcf
shahan <shahanverse@gmail.com>
2026-05-04 21:42:23 +0530

    f

--- client/src/pages/freelancer/FreelancerDashboard.jsx
+++ client/src/pages/freelancer/FreelancerDashboard.jsx
@@ -1,6 +1,62 @@
-import React from 'react'
+import { useActionState, useEffect, useState } from "react"
+import { useSelector } from "react-redux"
+import Navbar from "../../components/Navbar"
+import axiosInstance from "../../utils/axiosInstance.js"
 
 const FreelancerDashboard = () => {
+
+  const {user} = useSelector((state) => state.auth)
+  const [gigs, setGigs] = useState([])
+  const [order, setOrders] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [showCreategig, setShowCreateGig] = useState(false)
+
+  const [gigForm., setGigForm] = useState ({
+    title: "",
+    description: "",
+    category: "Web Development",
+    price: "",
+    deliveryDays: ""
+  })
+
+  const [error, setError] = useState("")
+  const [success, setSuccess] = useState("")
+
+  const fetchData = async () => {
+    try {
+      const [gigRes, ordersRes] = await Promise.all([
+        axiosInstance.get("/gigs"),
+        axiosInstance.get("/orders/freelancer")
+      ])
+      const myGigs = gigsRes.data.gigs.filter(
+        (gig) => gig.freelancer?._id === user?._id ||
+        gig.freelancer === user?.id 
+      )
+
+      setGigs(myGigs)
+      setOrders(ordersRes.data.orders)
+    } catch (error) {
+      console.log(error);
+      
+    }finally {
+      setLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    const getData = async () => {
+      await fetchData()
+    }
+    getData()
+  }, [])
+
+  const handleGigChange = (e) => {
+
+    setGigForm({ ...gigForm, [e.target.name]: e.target.value })
+  }
+
+
+
   return (
     <div className='min-h-screen bg-gradient-to-br from-slate-900 via-blue-950 to-slate-900 flex items-center justify-center'>
        <h1 className="text-4xl font-black text-white">
